a882a433f07943e9a76c9feaf2b0a6df4e0b70e2
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-08-06 12:58:47 -0500

    feat: add fetch to graphql

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
+import { useEffect } from 'react'
 import './App.css'
 
 const App = () => {
+  useEffect(() => {
+    fetch('http://localhost:4000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query{
+            allPersons {
+              name
+              lastname
+              age
+              phone
+            }
+            personCount
+          }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  })
+
   return <>Hola mundo</>
 }
 
